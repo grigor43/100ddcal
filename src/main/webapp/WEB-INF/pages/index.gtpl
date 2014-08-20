dc6eaaf36493fbf866ee036001a58c25d735d031
Rahul Somasunderam <rsom@certifydatasystems.com>
2014-08-19 22:56:38 -0700

    Added individual scores

--- src/main/webapp/WEB-INF/pages/index.gtpl
+++ src/main/webapp/WEB-INF/pages/index.gtpl
@@ -1,15 +1,29 @@
 <html>
   <head><title>Home</title></head>
   <body>
-    <div>
+    <div id="updateTime" style="display: none">
       Last Updated <span id="lu"></span>
     </div>
-    <div id="chart">
+    <h3>Teams</h3>
+    <div id="chart"></div>
+    <h3>Individuals</h3>
+    <div id="individuals">
 
     </div>
 
+    <script id="member" type="text/html">
+    <div class="col-md-2">
+      <div style="border: 1px solid #000000; height: 144px; margin-left: -15px; padding-left: 15px">
+        <h4>{{name}}</h4>
+        <p>{{teamName}}</p>
+        <h4>{{score}}</h4>
+
+      </div>
+    </div>
+    </script>
 
     <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.7.0/moment.min.js"></script>
+    <script src="/js/ICanHaz.min.js"></script>
     <script src="/js/underscore.js"></script>
     <script src="/js/d3.min.js"></script>
     <script src="/js/c3.min.js"></script>
--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -20,7 +20,7 @@ function processData(data) {
   console.log(data.data);
   var teams = _.sortBy(data.data, function (team) {
     return parseInt(team.rank.replace(',', ''));
-  });
+  }).slice(0, 0+5);
   console.log(teams);
   var data1 = _.map(teams, function (team) {
     return parseInt(team.avgSteps.replace(',', ''))
@@ -32,10 +32,31 @@ function processData(data) {
   console.log(data1);
   console.log(names);
 
+  _.each(data.data, function(team){
+    _.each(team.members, function (member){
+      member.teamName = team.name;
+    });
+  });
+
+  var allMembers = _.flatten(_.collect(data.data, function (team){
+    return team.members;
+  }));
+
+  var sortedMembers = _.sortBy(allMembers, function(member){
+    var score = member.score == '-' ? '0' : member.score;
+    return - parseInt(score.replace(',',''));
+  }).slice(0, 0+6);
+  console.log(sortedMembers);
+
   plotData(data1, names);
   var theMoment = moment.utc(data.date);
 
   $('#lu').html(theMoment.fromNow());
+  $('#updateTime').show();
+  $('#individuals').html('')
+  _.each(sortedMembers, function(member){
+    $('#individuals').append(ich.member(member));
+  });
 
 }
 function reloadData() {
