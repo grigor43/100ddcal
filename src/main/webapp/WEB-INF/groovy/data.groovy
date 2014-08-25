263e794f6f682168995a93d4a8f1425d8a311224
Rahul Somasunderam <rahul.som@gmail.com>
2014-08-24 19:54:04 -0700

    Added id to json

--- src/main/webapp/WEB-INF/groovy/data.groovy
+++ src/main/webapp/WEB-INF/groovy/data.groovy
@@ -1,3 +1,4 @@
+import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 import h100.CacheHelper
 import h100.Report
@@ -12,19 +13,22 @@ List<Report> reports = CacheHelper.get('data') {
   }
 }
 
-log.info "${reports}"
+log.info "Available: ${reports.size()}"
 
 if (offset < reports.size()) {
 
   def report = reports[-offset]
-  log.info "JSON: ${new JsonSlurper().parseText(report.data)}"
+
+    def json = new JsonSlurper().parseText(report.data)
+    log.info "JSON: ${json}"
   log.info "report.data: ${report.data}"
   response.setHeader("Access-Control-Allow-Origin", "*")
   response.setHeader("Access-Control-Allow-Methods", "GET")
   response.setHeader("Access-Control-Allow-Credentials", "true")
   response.setHeader("Content-Type", "application/json")
-
-  out.print report.data
+  json.id = report.id
+  log.info "Id: ${report.id}"
+  out.print new JsonBuilder(json).toPrettyString()
 } else if (offset) {
   response.sendError(404)
 } else {
