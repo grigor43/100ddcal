33b3c92eef9da4f80057a516ab156a07e060cb5e
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-06-19 15:05:33 -0700

    Added bootstrap to setup local environment as a clone of prod.

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OFFSET=1
+CONT=1
+
+while [ $CONT ]; do
+    curl http://h100cal.appspot.com/api/data?offset=$OFFSET > /tmp/data.txt
+    STATUS=$(cat /tmp/data.txt| wc -c)
+    if [ $STATUS = 194 ]; then
+      CONT=0
+    else
+      curl -X POST -d @/tmp/data.txt -H "Content-Type:application/json" http://localhost:7000/store > /tmp/data.out
+      OFFSET=$((OFFSET + 1))
+    fi
+done
+
+echo "END OFFSET: $OFFSET"
--- src/main/webapp/WEB-INF/groovy/data.groovy
+++ src/main/webapp/WEB-INF/groovy/data.groovy
@@ -13,7 +13,12 @@ List<Report> reports = CacheHelper.get('data') {
 
 log.info "Available: ${reports.size()}"
 
-if (localMode) {
+if (offset < reports.size()) {
+    def report = reports[-offset]
+
+    def json = new JsonSlurper().parseText(report.data)
+    log.info "JSON: ${json}"
+    log.info "report.data: ${report.data}"
     response.setHeader("Access-Control-Allow-Origin", "*")
     response.setHeader("Access-Control-Allow-Methods", "GET")
     response.setHeader("Access-Control-Allow-Credentials", "true")
@@ -21,25 +26,9 @@ if (localMode) {
     if (offset > 0) {
         response.setHeader("Cache-Control", "public, max-age=14400")
     }
-    out.println "http://h100cal.appspot.com/api/data?offset=${offset}".toURL().text
+    json.id = report.id
+    log.info "Id: ${report.id}"
+    out.print new JsonBuilder(json).toPrettyString()
 } else {
-    if (offset < reports.size()) {
-        def report = reports[-offset]
-
-        def json = new JsonSlurper().parseText(report.data)
-        log.info "JSON: ${json}"
-        log.info "report.data: ${report.data}"
-        response.setHeader("Access-Control-Allow-Origin", "*")
-        response.setHeader("Access-Control-Allow-Methods", "GET")
-        response.setHeader("Access-Control-Allow-Credentials", "true")
-        response.setHeader("Content-Type", "application/json")
-        if (offset > 0) {
-            response.setHeader("Cache-Control", "public, max-age=14400")
-        }
-        json.id = report.id
-        log.info "Id: ${report.id}"
-        out.print new JsonBuilder(json).toPrettyString()
-    } else {
-        response.sendError(404)
-    }
+    response.sendError(404)
 }
