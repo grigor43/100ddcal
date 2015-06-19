9e8fd1aea6795db9c4f4c2895a1c66f8c9f3f048
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-06-19 15:50:11 -0700

    Better bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ CONT=1
 while [ $CONT ]; do
     curl http://h100cal.appspot.com/api/data?offset=$OFFSET > /tmp/data.txt
     STATUS=$(cat /tmp/data.txt| wc -c)
-    if [ $STATUS = 194 ]; then
+    if [ $STATUS -lt 250 ]; then
       CONT=0
     else
       curl -X POST -d @/tmp/data.txt -H "Content-Type:application/json" http://localhost:7000/store > /tmp/data.out
